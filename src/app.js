e3eff3f5434bd388d8526ea4dfc1f60ea94f2f68
pritam586 <pritamprajapati666@gmail.com>
2025-04-02 07:18:00 +0530

    api handller

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.urlencoded({extended:true, limit: "16kb"}))
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+// rouets import
+import userRouter from './routes/user.routes.js'
+
+
+// Route decleration
+app.use("/user",userRouter)
 export {app} 
