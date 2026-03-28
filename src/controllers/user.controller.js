1da359f7cd8b62fba5aee2d951487754fe908a73
pritam <pritamprajapati666@gmail.com>
2026-03-28 13:20:01 +0530

    update user detail controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -232,6 +232,60 @@ const getCurrentUser = asyncHandler(async(req,res)=>{
 })
 
 
+const updateAccountDetail = asyncHandler(async(req,res)=>{
+   const {fullname , email} = req.body;
+
+   if(!fullname && !email){
+    throw new ApiError(400 , "All feild are requiured");
+   }
+
+   const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        fullname,
+        email
+      }
+    },
+    {
+      new: true
+    }
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user , "Account detail is successfully "))
+})
+
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+  const avatarLocalPath = req.file?.path;
+  if(!avatarLocalPath){
+    throw new ApiError(400 , "Avatar file is missing");
+  }
+
+  const avatar   = await uploadOncloudinary(avatarLocalPath);
+
+  if(!avatar.url){
+    throw new ApiError(400 , "Error while uploading avatar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set:{
+        avatar:avatar.url
+      }
+    },
+    {
+      new : true
+    }
+  ).select("-password")
+
+ 
+})
+
+
+
 
 export {
   registerUser,
@@ -239,5 +293,8 @@ export {
   logOutUser,
   refreshAccessToken,
   changeCurrentPassword,
-  getCurrentUser
+  getCurrentUser,
+  updateAccountDetail,
+  updateUserAvatar
+
 }
