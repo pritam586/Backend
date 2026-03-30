808f9a52cdf36de5ee2e4c36d167c2be67869904
pritam <pritamprajapati666@gmail.com>
2026-03-30 11:35:23 +0530

    add getvideo functionality

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -99,11 +99,29 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    // ✅ validate ObjectId
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID");
+    }
+
+    // ✅ find video
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, video, "Video fetched successfully")
+    );
+
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+
+
 
 })
 
