ccf5c390fb0328866697929f585d7c1670b51a42
pritam <pritamprajapati666@gmail.com>
2026-04-01 11:12:57 +0530

    adding getvideoComment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,6 +8,15 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    page = parseInt(page)
+    limit = parseInt(limit)
+    const comments = await Comment.find({video : videoId})
+    const options = {
+        page,
+        limit
+    }
+    const result = await Comment.paginate({video : videoId}, options)
+    return res.status(200).json(new ApiResponse(200, result, "Comments fetched successfully"))
 
 })
 
