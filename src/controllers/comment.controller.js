07ec3524525de1c55257bf6309c51c0cc1cd37bd
pritam <pritamprajapati666@gmail.com>
2026-04-01 11:26:13 +0530

    delete commetn controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -49,6 +49,17 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params
+    const comment = await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(404, "Comment not found")
+    }
+    if (comment.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "Unauthorized to delete this comment");
+    }
+
+    await comment.deleteOne()
+    return res.status(200).json(new ApiResponse(200, null, "Comment deleted successfully"))
 })
 
 export {
